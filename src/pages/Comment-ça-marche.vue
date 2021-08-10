823481430c03557ae1dcd83cce22dac5e0bb8148
Adio7 <adioyaya6@gmail.com>
2021-08-10 19:08:35 +0100

    comment-ca-marche

--- src/pages/Comment-ça-marche.vue
+++ src/pages/Comment-ça-marche.vue
@@ -1,52 +1,209 @@
 <template>
   <Layout>
-    <div id="app" class="container">
-      <section>
-        <b-steps
-          v-model="activeStep"
-          :has-navigation="hasNavigation"
-          :icon-prev="prevIcon"
-          :icon-next="nextIcon"
-          :label-position="labelPosition"
-          :animated="isAnimated"
-          :rounded="isRounded"
-        >
-          <b-step-item step="1">
-            <h1 class="title has-text-centered">Step 1</h1>
-          </b-step-item>
+    <section class="container mx-auto">
+      <div class="has-text-centered mt-6">
+        <p class="title is-1 has-text-info">Comment ça marche ?</p>
+        <p class="subtitle is-6">African Startups Insights est une plateforme numérique spécialement <br/> dédiée aux entreprises africaines.</p>
+      </div>
 
-          <b-step-item step="2">
-            <h1 class="title has-text-centered">Step 2</h1>
-          </b-step-item>
+      <div class="columns my-6 mx-auto has-text-centered">
+        <div class="column">
+          <svg
+            width="80" 
+            height="80" 
+            viewBox="0 0 125 125" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle 
+              cx="62.5" 
+              cy="62.5" 
+              r="62.5" 
+              fill="#FF9B26"
+            />
+          </svg>
+          <p class="title is-5 has-text-info my-4">Recherchez votre domaine d’activité </p>
+          <p class="subtitle is-6 mt-6">Entrez dans la barre le domaine d’activité ou le pays de votre choix. ASI vous présente une liste de startups. </p>
+        </div>
+        <div class="column">
+          <svg
+            width="80" 
+            height="80" 
+            viewBox="0 0 125 125" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle 
+              cx="62.5" 
+              cy="62.5" 
+              r="62.5" 
+              fill="#FF9B26"
+            />
+          </svg>
+          <p class="title is-5 has-text-info my-4">Consultez la carte d’identité et l’influence des startups </p>
+          <p class="subtitle is-6 my-5">Analysez les performances et l’influence des startups de votre choix auprès de leurs utilisateurs</p>
+        </div>
+        <div class="column">
+          <svg
+            width="80" 
+            height="80" 
+            viewBox="0 0 125 125" 
+            fill="none" 
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle 
+              cx="62.5" 
+              cy="62.5" 
+              r="62.5" 
+              fill="#FF9B26"
+            />
+          </svg>
+          <p class="title is-5 has-text-info my-4">Contactez la ou les startup(s) de votre choix</p>
+          <p class="subtitle is-6 my-5">Retrouvez l’adresse et les contacts des  startups de votre choix et mettez vous en relation avec elles.</p>
+        </div>
+      </div>
 
-          <b-step-item step="3">
-            <h1 class="title has-text-centered">Step 3</h1>
-          </b-step-item>
-          <template v-if="customNavigation" #navigation="{ previous, next }">
-            <div class="has-text-centered is-centered">
-              <b-button
-                outlined
-                icon-pack="fas"
-                icon-left="arrow-left"
-                :disabled="previous.disabled"
-                @click.prevent="previous.action"
-              >
-                Précédent
-              </b-button>
-              <b-button
-                outlined
-                icon-pack="fas"
-                icon-right="arrow-right"
-                :disabled="next.disabled"
-                @click.prevent="next.action"
-              >
-                Suivant
-              </b-button>
+      <div>
+        <p class="title is-4 has-text-info">FAQs</p>
+
+        <b-collapse animation="slide" aria-id="contentIdForA11y3" :open="false">
+          <template #trigger="props">
+            <div
+              role="button"
+              class="
+                panel-heading
+                columns
+                is-variable is-mobile
+                has-background-white
+              "
+              aria-controls="contentIdForA11y3"
+            >
+              <div class="column is-10 has-text-justified margin-title">
+                <p class="has-text-info">Consultez la carte d’identité et l’influence des startups</p>
+              </div>
+              <div class="column is-2">
+                <b-button type="is-white" class="is-pulled-right">
+                  <figure class="image is-32x32">
+                    <g-image
+                      :class="{ rotate: props.open }"
+                      alt="faq"
+                      src="~/assets/faq/expend.svg"
+                      height="20"
+                    />
+                  </figure>
+                </b-button>
+              </div>
             </div>
           </template>
-        </b-steps>
-      </section>
-    </div>
+          <div
+            class="
+              panel-block
+              has-background-white
+              columns
+              is-multiline
+              has-background-white
+              is-centered
+              has-text-justified
+            "
+          >
+            <div class="column is-12" v-html="content"></div>
+          </div>
+        </b-collapse>
+        <b-collapse animation="slide" aria-id="contentIdForA11y3" :open="false">
+          <template #trigger="">
+            <div
+              role="button"
+              class="
+                panel-heading
+                columns
+                is-variable is-mobile
+                has-background-white
+              "
+              aria-controls="contentIdForA11y3"
+            >
+              <div class="column is-10 has-text-justified margin-title">
+                <p class="has-text-info">Consultez la carte d’identité et l’influence des startups</p>
+              </div>
+            </div>
+          </template>
+          <div
+            class="
+              panel-block
+              has-background-white
+              columns
+              is-multiline
+              has-background-white
+              is-centered
+              has-text-justified
+            "
+          >
+            <div class="column is-12" v-html="content"></div>
+          </div>
+        </b-collapse>
+        <b-collapse animation="slide" aria-id="contentIdForA11y3" :open="false">
+          <template #trigger="">
+            <div
+              role="button"
+              class="
+                panel-heading
+                columns
+                is-variable is-mobile
+                has-background-white
+              "
+              aria-controls="contentIdForA11y3"
+            >
+              <div class="column is-10 has-text-justified margin-title">
+                <p class="has-text-info">Consultez la carte d’identité et l’influence des startups</p>
+              </div>
+            </div>
+          </template>
+          <div
+            class="
+              panel-block
+              has-background-white
+              columns
+              is-multiline
+              has-background-white
+              is-centered
+              has-text-justified
+            "
+          >
+            <div class="column is-12" v-html="content"></div>
+          </div>
+        </b-collapse>
+        <b-collapse animation="slide" aria-id="contentIdForA11y3" :open="false">
+          <template #trigger="">
+            <div
+              role="button"
+              class="
+                panel-heading
+                columns
+                is-variable is-mobile
+                has-background-white
+              "
+              aria-controls="contentIdForA11y3"
+            >
+              <div class="column is-10 has-text-justified margin-title">
+                <p class="has-text-info">Consultez la carte d’identité et l’influence des startups</p>
+              </div>
+            </div>
+          </template>
+          <div
+            class="
+              panel-block
+              has-background-white
+              columns
+              is-multiline
+              has-background-white
+              is-centered
+              has-text-justified
+            "
+          >
+            <div class="column is-12" v-html="content"></div>
+          </div>
+        </b-collapse>
+      </div>
+    </section> 
   </Layout>
 </template>
 
