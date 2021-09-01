11599d6a6b31632bb2efc1ea53fe177821acb710
FleuryTIGRIOpenclassrooms <grosgras3@gmail.com>
2021-09-01 21:03:08 +0200

    Add startups overviews page contents

---
+++ src/pages/Overview.vue
@@ -0,0 +1,210 @@
+<template>
+  <Layout>
+    <section class="overview_page">
+        <header>
+            <figure>
+            </figure>
+            <aside>
+                <h1>RINTIO</h1>
+                <h2>Technologies et services de l’information</h2>
+                <p>
+                    Notre mission est d’accompagner nos clients et partenaires dans leur transformation 
+                    opérationnelle et organisationnelle.
+                </p>
+            </aside>
+        </header>
+        <section>
+            <ul>
+                <li>
+                    <figure>
+                        <figcaption>
+                            Date de création
+                            <strong>01-01-2018</strong>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <figcaption>
+                            Siège social
+                            <strong>Cotonou</strong>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <figcaption>
+                            Email
+                            <strong>contact@rintio.com</strong>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li>
+                    <figure>
+                        <figcaption>
+                            Site web
+                            <strong>
+                                <a href="www.rintio.com" title="Visiter le site">www.rintio.com</a>
+                            </strong>
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>
+            <section>
+                <section>
+                    <header>
+                        <figure>
+                            <figcaption>
+                                Capital social
+                                <span>2.000.000 FCFA</span>
+                            </figcaption>
+                        </figure>
+                    </header>
+                    <article>
+                        <figure>
+                            <figcaption>
+                                <strong>Active Users</strong>
+                                <span>(+23%)</span> than last week
+                            </figcaption>
+                        </figure>
+                        <ul>
+                            <li>
+                                <figure class="category">
+                                    <figcaption>Users</figcaption>
+                                </figure>
+                                <span>3,6K</span>
+                                <figure class="percent">
+                                    <figcaption></figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure class="category">
+                                    <figcaption>Clicks</figcaption>
+                                </figure>
+                                <span>2m</span>
+                                <figure class="percent">
+                                    <figcaption></figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure class="category">
+                                    <figcaption>Sales</figcaption>
+                                </figure>
+                                <span>$772</span>
+                                <figure class="percent">
+                                    <figcaption></figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure class="category">
+                                    <figcaption>Items</figcaption>
+                                </figure>
+                                <span>82</span>
+                                <figure class="percent">
+                                    <figcaption></figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+                    </article>
+                </section>
+                <section>
+                    <header>
+                        <figure>
+                            <figcaption>E-reputation</figcaption>
+                        </figure>
+                        <figure class="switch">
+                            <input type="checkbox">
+                            <figcaption class="slider_checker"></figcaption>
+                        </figure>
+                    </header>
+                    <article>
+                        <ul>
+                            <li>
+                                <figure> 
+                                    <figcaption>8k</figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure> 
+                                    <figcaption>10k</figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure> 
+                                    <figcaption>37k</figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure> 
+                                    <figcaption>17k</figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+                        <figure>
+                            <figcaption>
+                                Total
+                                <span>+18,15%</span>
+                            </figcaption>
+                        </figure>
+                    </article>
+                    <footer>
+                        <ul>
+                            <li>
+                                <figure>
+                                    <figcaption>
+                                        LINKEDIN
+                                        <span>+ 27,13%</span>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure>
+                                    <figcaption>
+                                        FACEBOOK
+                                        <span>+ 33,22%</span>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure>
+                                    <figcaption>
+                                        TWITTER
+                                        <span>+ 17,13%</span>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            <li>
+                                <figure>
+                                    <figcaption>
+                                        INSTAGRAM
+                                        <span>+ 25,13%</span>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                        </ul>
+                    </footer>
+                </section>
+            </section>
+        </section>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Aperçu",
+    meta: [
+      {
+        name: "Aperçu",
+        content: "Aperçu de la Startup",
+      },
+    ],
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../variables.scss"; 
+
+</style>
