de680cc6a920f034f265e44c7a7989f91460ac28
FleuryTIGRIOpenclassrooms <grosgras3@gmail.com>
2021-09-02 18:05:29 +0200

    Add style to overview startup's page

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,105 +1,133 @@
 <template>
   <Layout>
-    <section class="overview_page">
-        <header>
-            <figure>
+    <section class="overview_page px-6">
+        <header class="is-flex is-align-items-center my-5 card about_us" >
+            <figure class="ml-4 mr-2 p-0">
+               <img src="../assets/logo_rintio.png" title="RINTIO" alt="Logo RINTIO" />
             </figure>
-            <aside>
-                <h1>RINTIO</h1>
-                <h2>Technologies et services de l’information</h2>
-                <p>
+            <aside class="column is-three-quarters">
+                <h1 class="m-0 is-size-4">RINTIO</h1>
+                <h2 class="m-0 is-size-6 is-inline-block category py-2 px-4 pr-5">Technologies et services de l’information</h2>
+                <p class="is-size-6">
                     Notre mission est d’accompagner nos clients et partenaires dans leur transformation 
                     opérationnelle et organisationnelle.
                 </p>
             </aside>
         </header>
         <section>
-            <ul>
-                <li>
-                    <figure>
-                        <figcaption>
+            <ul class="is-flex is-justify-content-space-between	general_informations my-5">
+                <li class="card pl-3 pr-5 pt-4 pb-3">
+                    <figure class="is-flex is-flex-direction-row-reverse is-justify-content-space-between">
+                        <span class="is-flex is-align-items-center representation">
+                            <img src="../assets/general_informations/calendar.svg" title="" alt="" />
+                        </span>
+                        <figcaption class="is-flex is-flex-direction-column">
                             Date de création
                             <strong>01-01-2018</strong>
                         </figcaption>
                     </figure>
                 </li>
-                <li>
-                    <figure>
-                        <figcaption>
+                <li class="card pl-3 pr-5 pt-4 pb-3">
+                    <figure class="is-flex is-flex-direction-row-reverse is-justify-content-space-between">
+                        <span class="is-flex is-align-items-center representation">
+                            <img src="../assets/general_informations/locate.svg" title="" alt="" />
+                        </span>
+                        <figcaption class="is-flex is-flex-direction-column">
                             Siège social
                             <strong>Cotonou</strong>
                         </figcaption>
                     </figure>
                 </li>
-                <li>
-                    <figure>
-                        <figcaption>
+                <li class="card pl-3 pr-5 pt-4 pb-3">
+                    <figure class="is-flex is-flex-direction-row-reverse is-justify-content-space-between">
+                        <span class="is-flex is-align-items-center representation">
+                            <img src="../assets/general_informations/mail.svg" title="" alt="" />
+                        </span>
+                        <figcaption class="is-flex is-flex-direction-column">
                             Email
                             <strong>contact@rintio.com</strong>
                         </figcaption>
                     </figure>
                 </li>
-                <li>
-                    <figure>
-                        <figcaption>
+                <li class="card pl-3 pr-5 pt-4 pb-3">
+                    <figure class="is-flex is-flex-direction-row-reverse is-justify-content-space-between">
+                        <span class="is-flex is-align-items-center representation">
+                            <img src="../assets/general_informations/site_web.svg" title="" alt="" />
+                        </span>
+                        <figcaption class="is-flex is-flex-direction-column">
                             Site web
                             <strong>
-                                <a href="www.rintio.com" title="Visiter le site">www.rintio.com</a>
+                                <a href="www.rintio.com" title="Visiter le site" class="has-text-black">www.rintio.com</a>
                             </strong>
                         </figcaption>
                     </figure>
                 </li>
             </ul>
-            <section>
-                <section>
-                    <header>
-                        <figure>
-                            <figcaption>
+            <section class="is-flex is-justify-content-space-between about_startup mb-6">
+                <section class="contents_group ">
+                    <header class="card social_capital mb-5">
+                        <figure class="is-flex is-flex-direction-row-reverse is-justify-content-space-between p-4">
+                            <span class="is-flex is-align-items-center representation">
+                                <img src="../assets/social_capital/social_capital.svg" title="" alt="" />
+                            </span>
+                            <figcaption class="is-flex is-flex-direction-column is-justify-content-space-around">
                                 Capital social
-                                <span>2.000.000 FCFA</span>
+                                <span class="is-size-4 has-text-weight-bold">2.000.000 FCFA</span>
                             </figcaption>
                         </figure>
                     </header>
-                    <article>
+                    <article class="card p-5">
                         <figure>
-                            <figcaption>
-                                <strong>Active Users</strong>
+                            <figcaption class="is-size-7">
+                                <strong>Active Users</strong> <br/>
                                 <span>(+23%)</span> than last week
                             </figcaption>
                         </figure>
-                        <ul>
+                        <ul class="is-flex is-justify-content-space-between pt-6">
                             <li>
-                                <figure class="category">
+                                <figure class="category is-flex is-size-7">
+                                    <span class="is-flex mr-1">
+                                        <img src="../assets/category/users.svg" title="" alt="" />
+                                    </span>
                                     <figcaption>Users</figcaption>
                                 </figure>
-                                <span>3,6K</span>
+                                <span class="is-size-5 has-text-weight-bold">3,6K</span>
                                 <figure class="percent">
                                     <figcaption></figcaption>
                                 </figure>
                             </li>
                             <li>
-                                <figure class="category">
+                                <figure class="category is-flex is-size-7">
+                                    <span class="is-flex mr-1">
+                                        <img src="../assets/category/clicks.svg" title="" alt="" />
+                                    </span>
                                     <figcaption>Clicks</figcaption>
                                 </figure>
-                                <span>2m</span>
+                                <span class="is-size-5 has-text-weight-bold">2m</span>
                                 <figure class="percent">
                                     <figcaption></figcaption>
                                 </figure>
                             </li>
                             <li>
-                                <figure class="category">
+                                <figure class="category is-flex is-size-7">
+                                    <span class="is-flex mr-1">
+                                        <img src="../assets/category/sales.svg" title="" alt="" />
+                                    </span>
                                     <figcaption>Sales</figcaption>
                                 </figure>
-                                <span>$772</span>
+                                <span class="is-size-5 has-text-weight-bold">$772</span>
                                 <figure class="percent">
                                     <figcaption></figcaption>
                                 </figure>
                             </li>
                             <li>
-                                <figure class="category">
+                                <figure class="category is-flex is-size-7">
+                                    <span class="is-flex mr-1">
+                                        <img src="../assets/category/items.svg" title="" alt="" />
+                                    </span>
                                     <figcaption>Items</figcaption>
                                 </figure>
-                                <span>82</span>
+                                <span class="is-size-5 has-text-weight-bold">82</span>
                                 <figure class="percent">
                                     <figcaption></figcaption>
                                 </figure>
@@ -107,15 +135,18 @@
                         </ul>
                     </article>
                 </section>
-                <section>
-                    <header>
-                        <figure>
+                <section class="card contents_group p-5">
+                    <header class="is-flex is-justify-content-space-between">
+                        <figure class="is-flex is-align-items-center">
+                            <span class="is-flex representation p-4 mr-3">
+                                <img src="../assets/\social_networks/awards.svg" title="" alt="" />
+                            </span>
                             <figcaption>E-reputation</figcaption>
                         </figure>
-                        <figure class="switch">
-                            <input type="checkbox">
-                            <figcaption class="slider_checker"></figcaption>
-                        </figure>
+                        <label class="switch">
+                            <input type="checkbox" checked>
+                            <span class="slider round"></span>
+                        </label>
                     </header>
                     <article>
                         <ul>
@@ -148,37 +179,25 @@
                         </figure>
                     </article>
                     <footer>
-                        <ul>
-                            <li>
+                        <ul class="report_social_network is-size-7">
+                            <li class="is-flex is-align-items-center linkedin mt-2">
                                 <figure>
-                                    <figcaption>
-                                        LINKEDIN
-                                        <span>+ 27,13%</span>
-                                    </figcaption>
+                                    <figcaption> LINKEDIN<span class="rate mr-5 pr-5">+ 27,13%</span></figcaption>
                                 </figure>
                             </li>
-                            <li>
+                            <li class="is-flex is-align-items-center facebook mt-2">
                                 <figure>
-                                    <figcaption>
-                                        FACEBOOK
-                                        <span>+ 33,22%</span>
-                                    </figcaption>
+                                    <figcaption> FACEBOOK<span class="rate mr-5 pr-5">+ 33,22%</span></figcaption>
                                 </figure>
                             </li>
-                            <li>
+                            <li class="is-flex is-align-items-center twitter mt-2">
                                 <figure>
-                                    <figcaption>
-                                        TWITTER
-                                        <span>+ 17,13%</span>
-                                    </figcaption>
+                                    <figcaption>TWITTER <span class="rate mr-5 pr-5">+ 17,13%</span></figcaption>
                                 </figure>
                             </li>
-                            <li>
+                            <li class="is-flex is-align-items-center instagram mt-2">
                                 <figure>
-                                    <figcaption>
-                                        INSTAGRAM
-                                        <span>+ 25,13%</span>
-                                    </figcaption>
+                                    <figcaption> INSTAGRAM<span class="rate mr-5 pr-5">+ 25,13%</span> </figcaption>
                                 </figure>
                             </li>
                         </ul>
@@ -205,6 +224,126 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import "../variables.scss"; 
-
+    @import "../variables.scss"; 
+    $color_primary:#267EC3;
+    .about_us,.general_informations li{
+        box-shadow: 0px 0px 27px rgba(0, 0, 0, 0.05);
+    }
+    //About us
+    .about_us img{
+        height: 115px;
+        margin-bottom: -5px;
+    }
+    .about_us .category{
+        color: #FF9B26;
+        background: rgba(255, 155, 38, 0.15);
+        border-radius: 7px;
+    }
+    //Image
+    .representation{
+        background-color: $color_primary;
+        padding: 10px;
+        border-radius: 8px;
+    }
+    //General informations
+    .general_informations li{
+        @extend .card;
+        width: calc(((100vw - (3rem * 2))/4) - (10px * 6)); 
+        // 100vw : total width; 3rem *2 : padding (p-6) ; 4 : blocks; 10px : space; 6 (two spaces let by each partie)
+    }
+    .general_informations .representation img{
+        width: 20px;
+    }
+    //about_startup
+    .about_startup .contents_group{
+        width: calc(((100vw - (3rem * 2))/2) - 40px); //40px : space*4 (10px *4) sum of each space let
+    }
+    .social_capital{
+        // box-shadow: 0px 20px 27px rgba(0, 0, 0, 0.05);
+    }
+    .social_capital .representation img {
+        height: 80px;
+    }
+    .about_startup .category img{
+        height: 50px;
+    }
+    .about_startup .category figcaption{
+        padding-top: 20px;
+    }
+    //toogle from w3school
+        .switch {
+            position: relative;
+            display: inline-block;
+            width: 60px;
+            height: 20px;
+        }
+        .switch input { 
+            opacity: 0;
+            width: 0;
+            height: 0;
+        }
+        .slider {
+            position: absolute;
+            cursor: pointer;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background-color: #ccc;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+        .slider:before {
+            position: absolute;
+            content: "";
+            height: 30px;
+            width: 30px;
+            margin-top: -5px;
+            background-color: white;
+            -webkit-transition: .4s;
+            transition: .4s;
+        }
+        input:checked + .slider {
+            background-color: $color_primary;
+        }
+        input:focus + .slider {
+            box-shadow: 0 0 1px $color_primary;
+        }
+        input:checked + .slider:before {
+            -webkit-transform: translateX(30px);
+            -ms-transform: translateX(30px);
+            transform: translateX(30px);
+        }
+        /* Rounded sliders */
+        .slider.round {
+            border-radius: 34px;
+        }
+        .slider.round:before {
+            border-radius: 50%;
+        }
+    // Report social network
+    .report_social_network .rate{
+        right: 0;
+        position: absolute;
+        color: #FF9B26;
+    }
+    .report_social_network li::before {
+        content: "";
+        height: 10px;
+        width: 10px;
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+    .report_social_network li.linkedin::before {
+        background: linear-gradient(#76FAC7, #00D5D9);
+    }
+    .report_social_network li.facebook::before {
+        background: linear-gradient(#FF00E5, #7A00D9);
+    }
+    .report_social_network li.twitter::before {
+        background: linear-gradient(#267EC3, #0038FF);
+    }
+    .report_social_network li.instagram::before {
+        background: linear-gradient(#F9F871, #FF9B26);
+    }
 </style>
